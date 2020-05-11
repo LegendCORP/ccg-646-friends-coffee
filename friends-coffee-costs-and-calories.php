bd64617f5fed2950ab2b82810dbf63c1afd0eff9
johnnyborjomi <johnnyborjomi@gmail.com>
2020-05-11 19:35:24 +0300

    assets

--- friends-coffee-costs-and-calories.php
+++ friends-coffee-costs-and-calories.php
@@ -5,18 +5,40 @@
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Top 5 Real Money Online Poker Sites ($year) - CardsChat.com&trade;</title>
-        <meta name="description" content="Compare Leading Real Money Poker Sites > Read Brand Reviews &amp; Play Authentic Poker Games: Texas Hold&apos;em, Omaha, Omaha Hi-Lo. Get Exclusive Bonuses!" />
+        <title>How much coffee did the Friends characters really drink?</title>
+        <meta name="description" content="We worked out exactly how much money each of the Friends characters would have spent on coffee over all 10 seasons and how many calories they consumed." />
         <link
             href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,600&display=swap"
             rel="stylesheet"
         />
-       <link media="all" rel="stylesheet" href="/Sources/Css/pages/marketing/friends-coffee-costs-and-calories/main.css">
+       <link media="all" rel="stylesheet" href="dist/bundle.css">
     </head>
 
     <body>
-        Friends Coffee here
-        <?php echo 'test'; ?>
+        <div id="wrapper">
+        <header class="header">
+            <div class="container header__container">
+                <a href="/" class="header__logo-link">
+                    <img class="header__logo-img" src="dist/images/logo.png" alt="CardsChat.com">
+                </a>
+                <ul class="social-links social-links--header">
+                    <li class="social-links__item">
+                        <a href="https://twitter.com/intent/tweet?text=I%20just%20scored%200%20in%20this%20Texas%20Hold%27em%20Interactive%20%23Poker%20%23Quiz%20–%20think%20your%20worthy%20of%20a%20final%20table%20spot?%20https%3A%2F%2Fwww.cardschat.com%2Fwhich-hand-wins%2F%20♣️%20♥️%20♠️"
+                            class="social-links__link icon-twitter js-game-share-top-link" data-social-links="twitter"
+                            target="_blank" rel="noopener noreferrer"></a>
+                    </li>
+                    <li class="social-links__item">
+                        <a href="//www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.cardschat.com%2Fwhich-hand-wins&quote=I%20just%20scored%200%20in%20this%20Texas%20Hold%27em%20Interactive%20%23Poker%20%23Quiz%20%E2%80%93%20think%20your%20worthy%20of%20a%20final%20table%20spot%3F%20https%3A%2F%2Fwww.cardschat.com%2Fwhich-hand-wins%2F%20♣️%20♥️%20♠️"
+                            class="social-links__link icon-facebook js-game-share-top-link" data-social-links="facebook"
+                            target="_blank" rel="noopener noreferrer"></a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+        <section class="container">
+            <h1 class="text-center text-white">How much each Friends characters would spend on their coffees today and how many calories they would have consumed</h1>
+        </section>
+        </div>
         <script src="dist/js/pages/friends-coffee-costs-and-calories.js"></script> 
     </body>
 </html>
